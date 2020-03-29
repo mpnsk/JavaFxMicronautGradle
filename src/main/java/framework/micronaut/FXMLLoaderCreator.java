fca9c4adf1a21bc0f39ef89424829436cd139bca
user <>
2020-03-29 03:17:04 +0200

    Add javadoc

--- src/main/java/framework/micronaut/FXMLLoaderCreator.java
+++ src/main/java/framework/micronaut/FXMLLoaderCreator.java
@@ -7,6 +7,9 @@ import javafx.fxml.FXMLLoader;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+/**
+ * Creates a new FXMLLoader with the micronaut context already set to reduce repetition
+ */
 @Singleton
 public class FXMLLoaderCreator {
     @Inject
