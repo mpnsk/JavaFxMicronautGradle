040f727ef7844e9aff30ce430c798d63d860fdc6
user <>
2020-03-29 03:08:01 +0200

    Change scope and make printlines more obvious

--- src/main/java/randomBeans/MyBean.java
+++ src/main/java/randomBeans/MyBean.java
@@ -2,10 +2,12 @@ package randomBeans;
 
 import io.micronaut.context.annotation.Prototype;
 
+import javax.inject.Singleton;
+
 @Prototype
 public class MyBean {
     public MyBean() {
-        System.out.println("MyBean.MyBean");
+        System.out.println("MyBean constructor");
         this.description = "random bean instantiated by Micronaut";
     }
 
--- src/main/java/secondController/SecondController.java
+++ src/main/java/secondController/SecondController.java
@@ -7,16 +7,21 @@ import firstController.FirstController;
 import randomBeans.MyBean;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-@Prototype
+@Singleton
 public class SecondController implements Initializable {
     public Label label;
     @Inject
     FirstController controller;
     @Inject
     MyBean myBean;
+
+    public SecondController() {
+        System.out.println("SecondController constructor");
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
